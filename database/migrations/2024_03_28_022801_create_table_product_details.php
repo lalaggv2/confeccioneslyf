498e5b3007aa528a7d1ebd077841161bf8dca7c9
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-27 22:41:34 -0500

    [ADD] Add migrations all database

---
+++ database/migrations/2024_03_28_022801_create_table_product_details.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('sku');
+            $table->string('barcode');
+            $table->string('size');
+            $table->string('color');
+            $table->string('material');
+            $table->string('location');
+            $table->decimal('price', 12, 2);
+            $table->integer('stock');
+            $table->date('date_manufactured');
+            $table->text('notes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
---
+++ database/migrations/2024_03_28_022802_create_table_suppliers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('document_type');
+            $table->string('document');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
